4cd731246c2fb210c076f762ffca1a1400575e6b
Prabal1017 <Prabal1017.be21@chitkara.edu.in>
2025-01-27 02:13:28 +0530

    made first section

---
+++ script.js
@@ -0,0 +1,20 @@
+window.onload = function() {
+  // Automatically click the Home button after page load
+  document.getElementById('homeButton').click();
+};
+
+
+
+// document.getElementById('expandButton').addEventListener('click', function() {
+//   const list = document.getElementById('expandableList');
+//   const button = document.getElementById('expandButton');
+  
+//   // Toggle visibility of the list
+//   if (list.style.display === 'none' || list.style.display === '') {
+//       list.style.display = 'block'; // Show the list
+//       button.textContent = 'Collapse'; // Change button text
+//   } else {
+//       list.style.display = 'none'; // Hide the list
+//       button.textContent = 'Expand'; // Change button text
+//   }
+// });
