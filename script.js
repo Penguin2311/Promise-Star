cfacfa06d7fe7e34463afc18d9f5c4ddc2d34ef7
Prabal1017 <Prabal1017.be21@chitkara.edu.in>
2025-01-27 22:11:58 +0530

    major changes

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-window.onload = function() {
-  // Automatically click the Home button after page load
-  document.getElementById('homeButton').click();
-};
 
 
-
-// document.getElementById('expandButton').addEventListener('click', function() {
-//   const list = document.getElementById('expandableList');
-//   const button = document.getElementById('expandButton');
-  
-//   // Toggle visibility of the list
-//   if (list.style.display === 'none' || list.style.display === '') {
-//       list.style.display = 'block'; // Show the list
-//       button.textContent = 'Collapse'; // Change button text
-//   } else {
-//       list.style.display = 'none'; // Hide the list
-//       button.textContent = 'Expand'; // Change button text
-//   }
+// // Close button functionality
+// closeButton.addEventListener('click', () => {
+//   overlay.style.display = 'none'; // Hide overlay
 // });
+
+
+// function lmao(){
+//   document.querySelector(".lmao").style.display = "block";
+
+// }
